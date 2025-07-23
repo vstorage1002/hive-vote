3275f00a39872d6ae527221620bbfabfc9cb1aaa
vstorage1002 <vstorage1002@gmail.com>
2025-07-23 11:24:36 +0800

    Update payout.js

--- scripts/payout.js
+++ scripts/payout.js
@@ -11,7 +11,7 @@ const REWARD_CACHE_FILE = 'ui/reward_cache.json';
 const PAYOUT_LOG_FILE = 'ui/payout.log';
 const DELEGATION_HISTORY_FILE = 'delegation_history.json';
 const MIN_PAYOUT = 0.001;
-const IS_DRY_RUN = true; // 🔁 Set to false for real payouts : SET TO TRUE FOR DRY RUN TESTING
+const IS_DRY_RUN = false; // 🔁 Set to false for real payouts : SET TO TRUE FOR DRY RUN TESTING
 
 const API_NODES = [
   'https://api.hive.blog',
